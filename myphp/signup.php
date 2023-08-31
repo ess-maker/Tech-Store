0c24760f9278723c69f6b539e21f4866a949ff8e
mohamed <mohamedmersal98@gmail.com>
2023-08-31 19:59:09 +0300

    made sign up

--- myphp/signup.php
+++ myphp/signup.php
@@ -1,47 +1,51 @@
-<?php 
-    include 'api.php';
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: *");
-    header("Access-Control-Allow-Headers: *");
-    $data = json_decode(file_get_contents("php://input"));
+<?php
+include 'api.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+header("content-type: application/json");
 
-    $user = $_POST['username'];
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
-    $fullName = $_POST['fullName'];
-    
-    $filename = $_FILES["img"]["name"];
-    $tempname = $_FILES["img"]["tmp_name"];
-    $folder = "../../img/" . $filename;
+// Check if the POST request contains the 'username' field
+if (isset($_POST['username'])) {
+    // Get the values sent from React
+    @$username = $_POST['username'];
+    @$email = $_POST['email'];
+    @$password = $_POST['password'];
+    @$name = $_POST['name'];
+    @$lastname = $_POST['lastname'];
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (empty($email) || empty($user) || empty($pass) || empty($fullName)) {
-            echo json_encode(["EmptyErrMessage" => "Please Enter Fill All Fields"]);
+    @$filename = $_FILES["img"]["name"];
+    @$tempname = $_FILES["img"]["tmp_name"];
+    @$folder = "../img/" . $filename;
+    @$file_type = $_FILES['img']['type'];
+    $allowed = array("image/jpeg", "image/gif", "image/png");
+
+    // Check if the file type is not allowed
+    if (!(in_array($file_type, $allowed))) {
+        echo json_encode(["message" => 'Only jpg, gif, and png files are allowed.']);
+    } else {
+        // Move the uploaded file to the destination folder
+        if (move_uploaded_file($tempname, $folder)) {
         } else {
-            $sql_sel = "SELECT * FROM signup  WHERE email = '$email'";
-            $res = $conn->query($sql_sel);
-            $sql_select = "SELECT * FROM signup WHERE username = '$user'";
-            $result = $conn->query($sql_select);
-            
-            if ($res->num_rows > 0) {
-                echo json_encode(["EmailErrMessage" => "Email Already Exists"]);
-            } else if ($result->num_rows > 0) {
-                echo json_encode(["UsernameErrMessage" => "Username Already Exists"]);
+            echo json_encode(["message" => 'An error occurred while uploading the file. Please try again.']);
+        }
+        if (empty($username) || empty($email) || empty($password) || empty($name) || empty($lastname)) {
+            echo json_encode(["message" => 'Please Enter A username']);
+        }
+        $select = "SELECT * FROM signup WHERE email = '$email' OR username =  '$username'";
+        $res = $conn->query($select);
+        if ($res->num_rows > 0) {
+            echo json_encode(["message" => 'Email Or username Already Exists']);
+        } else {
+            $sql = "INSERT INTO signup (username, email, password, name,lastname, img) VALUES ('$username', '$email', '$password', '$name','$lastname', '$filename')";
+            if ($conn->query($sql) === TRUE) {
+                echo json_encode(["message" => "Registration successful"]);
             } else {
-                if (move_uploaded_file($tempname, $folder)) {
-                    $sql = "INSERT INTO signup (username,email,password,fullname,img) VALUES ('$user','$email','$pass','$fullName','$filename')";
-                    if ($conn->query($sql) === TRUE) {
-                        echo json_encode(["message" => "Registration successful"]);
-                    } else {
-                        echo json_encode(["error" => "Registration failed"]);
-                    }
-                } else {
-                    echo json_encode(["error" => "File upload failed"]);
-                }
+                echo json_encode(["message" => "Database error: " . $conn->error]);
             }
         }
     }
-    
-?>
-
+} else {
+    echo json_encode(["error" => "No data received"]);
+}
 
--- src/componante/login and singup/Singup.tsx
+++ src/componante/login and singup/Singup.tsx
@@ -1,124 +1,135 @@
-import { FormEvent, useState } from "react";
-import "./login.scss";
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+import './signup.css';
 
-interface FormDataSignup {
-  username: string;
-  email: string;
-  password: string;
-  fullName: string;
-  img: File | null;
-}
+function SignUpForm() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewImage, setPreviewImage] = useState<string | null>('./unknown-person-icon-27.jpg');
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-const Singup = () => {
-  const [img, setImg] = useState<File | null>(null);
-  const [msg , setmsg] = useState<string>("")
-  console.log(msg);
-  
-  const [formData, setFormData] = useState<FormDataSignup>({
-    username: "",
-    email: "",
-    password: "",
-    fullName: "",
-    img: img,
-  });
-
-  if (formData.email !== "" && formData.password !== "" ) {
-    const url = "Localhost:8000/signup.php"
-    const headers = {
-      "Accept":"application/json",
-      "Content-type": "appliction/json"
-    }
-    fetch(url , {
-      method : "POST",
-      headers:headers,
-      body:JSON.stringify(formData)
-    } 
-      ).then (res => res.json())
-      .then (res => {
-        if (res[0].result !== "Invalid username" || "") {
-          console.log("hamza ");
-        }
-        setmsg(res[0].result)
-      }).catch(err => {
-        console.log(err);
-      })
-  }
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    // Handle form submission
-    console.log("Form submitted");
-    console.log("username:", formData.username);
-    console.log("Email:", formData.email);
-    console.log("Password:", formData.password);
-    console.log("fullName:", formData.fullName);
-    console.log(img);
-fetch('Localhost:8000/signup.php')
-    // Reset form fields
-    setFormData({
-      username: "",
-      email: "",
-      password: "",
-      fullName: "",
-      img: null,
-    });
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
   };
 
-  const handleImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleLastnameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLastname(e.target.value);
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
-      setImg(e.target.files[0]);
+      const file = e.target.files[0];
+      setSelectedFile(file);
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreviewImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
     }
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append('username', username);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('name', name);
+    formData.append('lastname', lastname);
+    if (selectedFile) {
+      formData.append('img', selectedFile);
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8000/signup.php', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      if (response.data.error) {
+        console.log("error");
+      } else {
+        setMessage(response.data.message);
+        console.log(response.data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    setIsLoading(false);
+
+    // Clear form fields
+    setUsername('');
+    setEmail('');
+    setPassword('');
+    setName('');
+    setLastname('');
+    setSelectedFile(null);
+    setPreviewImage('./unknown-person-icon-27.jpg');
   };
 
   return (
-    <div className="container">
-      <form method="post" className="main-form" onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          type="text"
-          name="username"
-          placeholder="Username"
-        />
-        <input
-          onChange={handleChange}
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          onChange={handleChange}
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
-        <input
-          onChange={handleChange}
-          type="text"
-          name="fullName"
-          placeholder="Full Name"
-        />
-        <label className="custom-file-upload">
-          <input
-            type="file"
-            name="file"
-            placeholder="hamza"
-            className="file-input"
-            onChange={handleImg}
-          />
-          Upload img
+    <div>
+      <h1>Sign Up</h1>
+
+      <form className='signup-container' encType='multipart/form-data' onSubmit={handleSubmit}>
+        <p>{message}</p>
+        <label htmlFor='pimg' className='profile-image-label'>
+          <div className='profile-image-container'>
+            <div className={`profile-image-wrapper ${isLoading ? 'loading' : ''}`}>
+              <div className='loading-spinner'></div>
+              {isLoading ? (
+                <div className='loading-message'>Loading...</div>
+              ) : (
+                <img id='preview-image' className='preview-image' src={previewImage || './unknown-person-icon-27.jpg'} alt='Preview' />
+              )}
+            </div>
+          </div>
         </label>
-        <button type="submit">Submit</button>
+        <input required id='pimg' className='profile-image-input' type="file" onChange={handleFileChange} />
+        <label htmlFor='uinput'>Username:</label>
+        <input id='uinput' className='username-input' type="text" value={username} onChange={handleUsernameChange} />
+        <br />
+        <label>Email:</label>
+        <input className='email-input' type="email" value={email} onChange={handleEmailChange} />
+        <br />
+        <label>Password:</label>
+        <input className='password-input' type="password" value={password} onChange={handlePasswordChange} />
+        <br />
+        <label>First Name:</label>
+        <input className='first-name-input' type="text" value={name} onChange={handleNameChange} />
+        <br />
+        <label>Last Name:</label>
+        <input className='last-name-input' type="text" value={lastname} onChange={handleLastnameChange} />
+        <br />
+        <br />
+        <button className='submit-button' type="submit">
+          Sign Up
+        </button>
       </form>
     </div>
   );
-};
+}
 
-export default Singup;
+export default SignUpForm;
